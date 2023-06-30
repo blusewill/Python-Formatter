99464a14d5e8be6b0f4d838e81064949a79e8db2
blusewill <blusewillstudio@protonmail.com>
2023-06-30 17:17:05 +0800

    Update splitter and add following up Support for one file runner

---
+++ srt_to_transcript/start.py
@@ -0,0 +1,21 @@
+import pysrt
+
+# Get the filename of the SRT file from the user
+filename = input("Enter the filename of the SRT file: ")
+
+# Load the SRT file
+subs = pysrt.open(filename)
+
+# Create an empty string to store the transcript
+transcript = ''
+
+# Loop through each subtitle and add its text to the transcript
+for sub in subs:
+    transcript += sub.text + '\n'
+
+# Write the transcript to a file called output.txt
+with open('output.txt', 'w') as f:
+    f.write(transcript)
+
+# Print a message to confirm that the transcript was written to the file
+print("Transcript written to output.txt")
